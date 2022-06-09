e7a2919dbece446ec854062188a0ccf8bacf0ee1
mukhriddin <khodievm@gmail.com>
2022-06-09 16:55:17 +0500

    lesson-2

---
+++ lesson-1/script.js
@@ -0,0 +1,25 @@
+var ism="Mukhriddin";
+var yosh=24;
+yosh=25;
+var yosh=26;
+
+
+let job="developer";
+job="QA enginier";
+
+//let job="Mentor";
+
+let hobby="play football";
+
+
+
+
+const car='merc';
+// car="bmw";
+// const car="nexia";
+
+
+
+console.log(car);
+console.log(ism,yosh);
+console.log(job,hobby);

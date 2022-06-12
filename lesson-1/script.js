bf9bd55e33b25eee90de398d02754435438a84f5
mukhriddin <khodievm@gmail.com>
2022-06-12 17:15:55 +0500

    lesson3

--- lesson-1/script.js
+++ lesson-1/script.js
@@ -1,25 +1,64 @@
-var ism="Mukhriddin";
-var yosh=24;
-yosh=25;
-var yosh=26;
+// var ism="Mukhriddin";
+// var yosh=24;
+// yosh=25;
+// var yosh=26;
 
 
-let job="developer";
-job="QA enginier";
+// let job="developer";
+// job="QA enginier";
 
-//let job="Mentor";
+// //let job="Mentor";
 
-let hobby="play football";
-
-
+// let hobby="play football";
 
 
-const car='merc';
+
+
+// const car='merc';
 // car="bmw";
 // const car="nexia";
 
 
+// console.log(car);
+// console.log(ism,yosh);
+// console.log(job,hobby);
 
-console.log(car);
-console.log(ism,yosh);
-console.log(job,hobby);
+// 
+
+// 1. string 
+// 2. number
+// 3. boolean   true / false 
+// 4. undefined
+// 5. null
+
+
+// 6. bigInt
+// 7. symbol
+
+//8. object
+
+// let isname="Mukhriddin";
+// let num="123345";
+// let age='yoshim 24 da';
+// let hobby=`plaay football`;
+
+
+// console.log(typeof( hobby));
+// console.log(num);
+// console.log(age);
+
+// let number=-0.000000022;
+// console.log(number);
+// console.log(typeof(number));
+
+// console.log( typeof(10 < 100));
+
+
+
+// let myCar;
+// console.log(myCar);
+
+// let show=null;
+
+// console.log(show);
+

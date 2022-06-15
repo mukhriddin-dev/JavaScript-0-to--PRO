aab3ce190e58c0756654033f51438306b1ada3bc
mukhriddin <khodievm@gmail.com>
2022-06-15 20:27:29 +0500

    ok

---
+++ lesson-2/script.js
@@ -0,0 +1,45 @@
+// data type string
+
+// string
+
+let lastName="Mukhriddin";
+let surName="Khodiev";
+
+// string concatenation
+
+let fullName=lastName+" "+surName;
+
+console.log(fullName);
+
+
+// string characters
+
+console.log(lastName[9]);
+
+
+// string length
+
+
+console.log(fullName.length);
+
+
+// string methods
+
+
+const small=fullName.toUpperCase();
+
+
+console.log(lastName.indexOf('r'));
+
+
+
+
+
+
+
+
+
+
+
+
+
